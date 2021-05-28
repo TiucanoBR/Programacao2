ddb455ea2a3f3ba8f8d40c010c00aa87ebd46c71
Arthur Vieira Maia <arthur.gamer20@gmail.com>
2021-05-28 20:09:48 -0300

    Aula 28/05

---
+++ multiprocessing/BarraDeProgresso.java
@@ -0,0 +1,13 @@
+package multiprocessing;
+
+public class BarraDeProgresso implements Runnable {
+	public void progresso() {
+		//codigo que atualiza a barra de progresso
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		//codigo que atualiza a barra de progresso
+	}
+}
---
+++ multiprocessing/DesenhaMapa.java
@@ -0,0 +1,13 @@
+package multiprocessing;
+
+public class DesenhaMapa implements Runnable {
+	public void desenhar() {
+		//codigo para desenhar um mapa
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		//codigo para desenhar um mapa
+	}
+}
