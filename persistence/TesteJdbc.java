a460d3b35ceb7f6699d37cc3a872e11f75d674d0
Arthur Vieira Maia <arthur.gamer20@gmail.com>
2021-06-18 21:07:29 -0300

    Aula 18/06

---
+++ persistence/TesteJdbc.java
@@ -0,0 +1,21 @@
+package persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TesteJdbc {
+	public static void main(String[] args) {
+		try {
+			String url_con = "jdbc:mysql://127.0.0.1/test";
+			String user = "root";
+			String password = "root";
+			Connection con = DriverManager.getConnection(url_con, user, password);
+			System.out.println("OK, conectado ao banco");
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
